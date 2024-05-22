09707de05789fba597aa65f5d53851263a592ef4
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-22 12:40:36 +0300

    Change server url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener('submit', async e => {
     console.log('Sending data to a server...');
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/submit", {
+        const response = await fetch("https://fishka-server.onrender.com/submit", {
             method: "POST",
             body: data,
         });
